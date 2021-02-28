935c9b96de3e826298b770946c10beaa98f6f0bb
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-03-01 06:57:31 +0900

    Initial Commit

---
+++ cmd/web/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"bookings/pkg/config"
+	"bookings/pkg/handlers"
+	"bookings/pkg/render"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers((repo))
+
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Printf("Starting appllication on port %s\n", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"bookings/pkg/config"
+	"bookings/pkg/handlers"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	// mux.Use(middleware.Logger)
+	// mux.Use(writeToConsole)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	return mux
+}
