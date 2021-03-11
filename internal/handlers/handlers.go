bfcd35a1561f9c6fff23a05aa905e4650332286e
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-03-12 07:23:43 +0900

    feat: Added Form & Error struct to check if the form is valid but invalid

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -55,7 +55,13 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.go.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.go.tmpl", &models.TemplateData{
+		Form: nil,
+	})
+}
+
+// PostReservation handles the posting of a reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 }
 
 // Generals renders the room page
